e1d31a2e536e89fdc942761492963724a9d89756
zahid hussain <100913055+zahidhussain998@users.noreply.github.com>
2024-02-03 00:08:00 +0500

    Update BlogList.tsx

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -14,13 +14,22 @@ export const revalidate = 20;
 
 
 function BlogList({ posts }: Props) {
+    const [visiblePosts, setVisiblePosts] = useState(6); // State for visible posts
+
+
+
+    const loadMorePosts = () => {
+    setVisiblePosts((prevVisiblePosts) => prevVisiblePosts + 6);
+  };
+
+
   return (
     <div>
    
       <hr className="border-[#0A7DFF] md-10" />
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {/* post */}
-        {posts.map((post) => (
+        {filteredPosts.slice(0, visiblePosts).map((post) => (
           <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
             <div className="flex flex-col group cursor-pointer">
               <div
@@ -74,6 +83,26 @@ function BlogList({ posts }: Props) {
             </div>
           </ClientSideRoute>
         ))}
+              <div className="flex justify-center mb-10">
+  {filteredPosts.length > visiblePosts && (
+    <button
+      className="flex items-center bg-white text-black px-4 py-2 mt-4 rounded-xl"
+      onClick={loadMorePosts}
+    >
+      Load More
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-4 h-4 ml-2" 
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+      </svg>
+    </button>
+  )}
+</div>
       </div>
 
     </div>
