470608bc6c34af926ce591aa2cf4b67a87ed16a4
zahidhussain998 <zahidzahidhusssain@gmail.com>
2024-02-09 20:38:21 +0500

    zahid changes

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
             <div className="bg-primary w-full overflow-hidden">
               <div className={`sm:px-16 px-6 flex justify-center items-center`}>
                 <div className={`xl:max-w-[1280px] w-full`}>
-                  <link rel="icon" href="/icon.png" />
+                  <link rel="icon" href="/n.png" />
                   <Header />
                   <hr className="border-[#0A7DFF]"/>
                    {/* <div className="mt-5"> */}
