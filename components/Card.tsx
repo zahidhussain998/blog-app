6b4ed60bbe20d1e3dbdfcb5557d256304b07e83a
zahidhussain998 <zahidzahidhusssain@gmail.com>
2024-02-20 20:27:11 +0500

    card new

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,33 +1,27 @@
 import Link from "next/link";
 
-
-
-
-
 const Card = () => {
   return (
-    <div className="flex justify-start items-start  ">
-
-<div className="bg-white shadow-full rounded-md p-6 ">
-  <div className="relative mb-4 ">
-      <span className="text-gray-500 ">Promoted</span>
-      <hr/>
-      <img
-        className="h-30 w-20"
-        src="https://res.cloudinary.com/jerrick/image/upload/v1691336585/64cfbf898dc3fa001d604dc0.png"
-        alt="image"
-      />
-      <h2 className="text-xl font-semibold mb-2 text-black">
-        100% trusted freelancer
-      </h2>
-      <Link href="https://www.fiverr.com/zohaibzohaibhus?up_rollout=true">
-        <p className="text-black text-sm outline-slate-800">
-          zahid hussain is an website developer 100% trustable freelancer
-        </p>
-      </Link>
-
-  </div>
-    </div>
+    <div className="flex justify-start items-start">
+      <div className="bg-white shadow-full rounded-md p-6 border-4 border-blue-500">
+        <div className="relative mb-4">
+          <span className="text-gray-500">Promoted</span>
+          <hr />
+          <img
+            className="h-30 w-20"
+            src="https://res.cloudinary.com/jerrick/image/upload/v1691336585/64cfbf898dc3fa001d604dc0.png"
+            alt="image"
+          />
+          <h2 className="text-xl font-semibold mb-2 text-black">
+            100% trusted freelancer
+          </h2>
+          <Link href="https://www.fiverr.com/zohaibzohaibhus?up_rollout=true">
+            <p className="text-black text-sm outline-slate-800">
+              zahid hussain is an website developer 100% trustable freelancer
+            </p>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
